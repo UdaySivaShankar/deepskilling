7c9cd3b40db7b221a1d2ff0553bcb149e56248fb
udays <udaysivashankar@gmail.com>
2026-07-28 15:31:00 +0530

    All done

--- week-6/hands-on-10/student-course-portal/src/app/guards/unsaved-changes-guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-
-export interface CanComponentDeactivate {
-  canDeactivate: () => boolean;
-}
-
-export const unsavedChangesGuard: CanDeactivateFn<CanComponentDeactivate> = (component) => {
-  return component.canDeactivate ? component.canDeactivate() : true;
-};
